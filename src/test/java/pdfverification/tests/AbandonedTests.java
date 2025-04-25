8709a75c1a316c2966a7489e73ca3822a4712f36
NathanielRichards1 <nathaniel.richards@dvsa.gov.uk>
2025-04-25 15:53:11 +0100

    feat(cb2-15739&16085): new defect tests for pdf

--- src/test/java/pdfverification/tests/AbandonedTests.java
+++ src/test/java/pdfverification/tests/AbandonedTests.java
@@ -39,6 +39,21 @@ public abstract class AbandonedTests {
     protected static String LOCATION_NUMBER;
     protected static String DATE_OF_THE_TEST;
     protected static String SECTION_TEXT;
+    protected static String DEFECT_ADDED_DURING_TEST_TEXT;
+    protected static String DO_NOT_DRIVE_UNTIL_REPAIRED_TEXT;
+    protected static String DANGEROUS_DEFECTS_TEXT_LINE1;
+    protected static String DANGEROUS_DEFECTS_TEXT_LINE2;
+    protected static String REPAIR_IMMEDIATELY_TEXT;
+    protected static String MAJOR_DEFECTS_TEXT_LINE1_1ST;
+    protected static String MAJOR_DEFECTS_TEXT_LINE1_2ND;
+    protected static String MAJOR_DEFECTS_TEXT_LINE2_2ND;
+    protected static String REPAIR_AS_SOON_AS_POSSIBLE_TEXT;
+    protected static String MINOR_DEFECTS_TEXT_LINE1;
+    protected static String MONITOR_AND_REPAIR_IF_NECESSARY_TEXT;
+    protected static String ADVISORY_DEFECTS_TEXT_LINE1;
+    protected static String DEFECTS_RECTIFIED_AT_TIME_OF_TEST_TEXT;
+    protected static String PRS_DEFECTS__TEXT_LINE1;
+    protected static String PRS_DEFECTS__TEXT_LINE2;
 
     public AbandonedTests() {
         this.htmlGenerator = new HtmlGenerator(new Handlebars());
@@ -50,13 +65,6 @@ public abstract class AbandonedTests {
     public void setup() throws Exception {
         pdfData = pdfGenerationService.generate(htmlGenerator.generate(testCertificate));
         pdfReader = pdfParser.readPdf(pdfData);
-
-        // write pdfData to file for debugging
-        try (FileOutputStream fos = new FileOutputStream("vtp12.pdf")) {
-            fos.write(pdfData);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     @Test
@@ -131,4 +139,64 @@ public abstract class AbandonedTests {
     public void verifyDate() throws IOException {
         assertTrue(pdfParser.getRawText(pdfReader, 3).contains(DATE_OF_THE_TEST));
     }
+
+    @Test
+    public void verifyDefectAddedDuringTestText() throws IOException {
+        assertTrue(pdfParser.getRawText(pdfReader, 1).contains(DEFECT_ADDED_DURING_TEST_TEXT));
+    }
+
+    @Test
+    public void verifyDoNotDriveUntilRepairedText() throws IOException {
+        assertTrue(pdfParser.getRawText(pdfReader, 1).contains(DO_NOT_DRIVE_UNTIL_REPAIRED_TEXT));
+    }
+
+    @Test
+    public void verifyDangerousDefectsText() throws IOException {
+        assertTrue(pdfParser.getRawText(pdfReader, 1).contains(DANGEROUS_DEFECTS_TEXT_LINE1));
+        assertTrue(pdfParser.getRawText(pdfReader, 1).contains(DANGEROUS_DEFECTS_TEXT_LINE2));
+    }
+
+    @Test
+    public void verifyRepairImmediatelyText() throws IOException {
+        assertTrue(pdfParser.getRawText(pdfReader, 1).contains(REPAIR_IMMEDIATELY_TEXT));
+    }
+
+    @Test
+    public void verifyMajorDefectsText() throws IOException {
+        assertTrue(pdfParser.getRawText(pdfReader, 1).contains(MAJOR_DEFECTS_TEXT_LINE1_1ST));
+        assertTrue(pdfParser.getRawText(pdfReader, 1).contains(MAJOR_DEFECTS_TEXT_LINE1_2ND));
+        assertTrue(pdfParser.getRawText(pdfReader, 1).contains(MAJOR_DEFECTS_TEXT_LINE2_2ND));
+    }
+
+    @Test
+    public void verifyRepairAsSoonAsPossibleText() throws IOException {
+        assertTrue(pdfParser.getRawText(pdfReader, 1).contains(REPAIR_AS_SOON_AS_POSSIBLE_TEXT));
+    }
+
+    @Test
+    public void verifyMinorDefects() throws IOException {
+        assertTrue(pdfParser.getRawText(pdfReader, 1).contains(MINOR_DEFECTS_TEXT_LINE1));
+    }
+
+    @Test
+    public void verifyMonitorAndRepairIfNecessaryText() throws IOException {
+        assertTrue(pdfParser.getRawText(pdfReader, 1).contains(MONITOR_AND_REPAIR_IF_NECESSARY_TEXT));
+    }
+
+    @Test
+    public void verifyAdvisoryDefectsText() throws IOException {
+        assertTrue(pdfParser.getRawText(pdfReader, 1).contains(ADVISORY_DEFECTS_TEXT_LINE1));
+    }
+
+    @Test
+    public void verifyDefectsRectifiedAtTimeOfTestText() throws IOException {
+        assertTrue(pdfParser.getRawText(pdfReader, 2).contains(DEFECTS_RECTIFIED_AT_TIME_OF_TEST_TEXT));
+    }
+
+    @Test
+    public void verifyPRSDefectsText() throws IOException {
+        assertTrue(pdfParser.getRawText(pdfReader, 2).contains(PRS_DEFECTS__TEXT_LINE1));
+        assertTrue(pdfParser.getRawText(pdfReader, 2).contains(PRS_DEFECTS__TEXT_LINE2));
+    }
+
 }
